868102e4e045c71c9ca2923b53bfe6c2783bcae3
Matheus Ponte <Matheuspontes894@outlook.com>
2022-08-24 20:50:59 -0300

    Instaland os cors

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,10 +1,12 @@
 import { Application } from "express";
 
 
+const cors =require('cors')
 const booksRoute = require('./booksRoutes.ts')
 const express = require('express')
 const app: Application = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/books',booksRoute);
 
